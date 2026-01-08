8868a0a7735a3c09f02ae51e3d49e7449ed9166b
soojinsong <162791828+soojinsong@users.noreply.github.com>
2026-01-09 02:01:12 +0900

    :sparkles: Feat: tailwindcss 세팅

---
+++ tailwind.config.ts
@@ -0,0 +1,26 @@
+import type { Config } from "tailwindcss";
+
+export default {
+  content: ["./index.html", "./src/**/*.{js,ts,jsx,tsx}"],
+  theme: {
+    extend: {
+      colors: {
+        point: {
+          main: "var(--color-point-main)",
+          sub: "var(--color-point-sub)",
+          back: "var(--color-point-back)",
+        },
+        gray: {
+          1: "var(--color-gray-1)",
+          2: "var(--color-gray-2)",
+          3: "var(--color-gray-3)",
+          4: "var(--color-gray-4)",
+          5: "var(--color-gray-5)",
+        },
+        white: "var(--color-white)",
+        black: "var(--color-black)",
+      },
+    },
+  },
+  plugins: [],
+} satisfies Config;
